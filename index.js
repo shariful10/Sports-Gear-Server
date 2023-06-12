a93c7a865cae19416efd19a631bfc5d108b89e5c
Sk Shariful Islam <skshariful10@gmail.com>
2023-06-12 21:14:22 +0600

    Get Classes Data From MongoDB Database By Using Get Method

--- index.js
+++ index.js
@@ -24,6 +24,15 @@ async function run() {
 	try {
 		// Connect the client to the server	(optional starting in v4.7)
 		await client.connect();
+
+		const classCollection = client.db("sportsDb").collection("classes");
+
+		// Classes Collection
+		app.get("/classes", async (req, res) => {
+			const result = await classCollection.find().toArray();
+			res.send(result);
+		});
+
 		// Send a ping to confirm a successful connection
 		await client.db("admin").command({ ping: 1 });
 		console.log("Pinged your deployment. You successfully connected to MongoDB!");
