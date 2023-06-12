b9ec94e4559a792f98c50962fd4f67e35261e784
Sk Shariful Islam <skshariful10@gmail.com>
2023-06-12 19:59:35 +0600

    Added Get Or Listen Methed For Running On Port

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
+const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors())
+app.use(cors());
 app.use(express.json());
+
+app.get("/", (req, res) => {
+	res.send("Sports Gear is running");
+});
+
+app.listen(port, () => {
+	console.log(`Sports Gear is running on port: ${port}`);
+});
