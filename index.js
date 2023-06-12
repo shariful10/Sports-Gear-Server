f60fbca5e8182d5f05f9507ba2c5cf231cfffcb5
Sk Shariful Islam <skshariful10@gmail.com>
2023-06-12 20:59:55 +0600

    User Name & Password Added On MongoDB Data

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri =
-	"mongodb+srv://<username>:<password>@cluster0.bq2ef3t.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.bq2ef3t.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
