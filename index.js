fff846e7555f80e9e425c6e1498aa7c0f3eda93b
Sk Shariful Islam <skshariful10@gmail.com>
2023-06-14 01:26:20 +0600

    Json Web Token Added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
