74e8259e39e19240ef72cce396f76206b31098a6
Sk Shariful Islam <skshariful10@gmail.com>
2023-06-13 19:56:34 +0600

    Get Users Data From Database By Using Get Method

--- index.js
+++ index.js
@@ -30,6 +30,11 @@ async function run() {
 		const instructorCollection = client.db("sportsDb").collection("instructors");
 
 		// User collection
+		app.get("/users", async (req, res) => {
+			const result = await userCollection.find().toArray();
+			res.send(result);
+		});
+
 		app.post("/users", async (req, res) => {
 			const user = req.body;
 			const query = { email: user.email };
