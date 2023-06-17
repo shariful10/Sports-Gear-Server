56b3fbb2ba171badb850781660936fd1f11fc147
Sk Shariful Islam <skshariful10@gmail.com>
2023-06-17 17:56:19 +0600

    Modify Some API

--- index.js
+++ index.js
@@ -178,6 +178,7 @@ async function run() {
 			res.send(result);
 		});
 
+		//Cart Collection
 		app.get("/carts/:email", async (req, res) => {
 			const email = req.params.email;
 			const result = await cartCollection.find({ email }).toArray();
@@ -188,6 +189,13 @@ async function run() {
 			const singleClass = req.body;
 			console.log(singleClass);
 			const result = await cartCollection.insertOne(singleClass);
+			res.send(result);
+		});
+
+		app.delete("/carts/:id", async (req, res) => {
+			const id = req.params.id;
+			const query = { _id: new ObjectId(id) };
+			const result = await cartCollection.deleteOne(query);
 			res.send(result);
 		});
 
